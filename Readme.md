0b52fd3cc288285a5189472bc352e93681cba5fd
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to create a context menu for header area
+
+
+<p>This example shows how to add custom items to the context menu displayed when an end-user right-clicks within the header area. A Test menu item is added. The GridMenu_Opened event hides the test item for all areas except DataArea.</p>
+
+<br/>
+
+
---
+++ VB/CreateValueContextMenu/MainWindow.xaml.vb
@@ -0,0 +1,35 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data
+Imports System.Data.OleDb
+Imports System.Windows
+Imports DevExpress.Xpf.PivotGrid
+Imports HowToBindToMDB.NwindDataSetTableAdapters
+Imports DevExpress.Xpf.PivotGrid.Internal
+
+Namespace HowToBindToMDB
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Private salesPersonDataTable As New NwindDataSet.SalesPersonDataTable()
+		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
+
+		Public Sub New()
+			InitializeComponent()
+			pivotGridControl1.DataSource = salesPersonDataTable
+			AddHandler pivotGridControl1.GridMenu.Opened, AddressOf GridMenu_Opened
+		End Sub
+
+		Private Sub GridMenu_Opened(ByVal sender As Object, ByVal e As System.EventArgs)
+			If pivotGridControl1.GridMenu.MenuType <> PivotGridMenuType.HeadersArea Then
+				Return
+			End If
+			TestItem.IsVisible = CType(pivotGridControl1.GridMenu.PlacementTarget.GetValue(FieldHeaders.AreaProperty), FieldArea) = FieldArea.DataArea
+		End Sub
+
+		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			salesPersonDataAdapter.Fill(salesPersonDataTable)
+		End Sub
+	End Class
+End Namespace
