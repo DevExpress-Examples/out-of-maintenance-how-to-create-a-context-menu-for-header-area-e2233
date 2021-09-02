9be1effa66b4ebc2ada2708b9c7df0700e7cb14a
DevExpressExampleBot <fakeEmail>
2021-09-02 18:08:02 +0300

    autogenerate VB

--- VB/CreateValueContextMenu/MainWindow.xaml.vb
+++ VB/CreateValueContextMenu/MainWindow.xaml.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Data
+﻿Imports System.Data
 Imports System.Data.OleDb
 Imports System.Windows
 Imports DevExpress.Xpf.PivotGrid
@@ -12,12 +11,14 @@ Namespace HowToBindToMDB
 	''' </summary>
 	Partial Public Class MainWindow
 		Inherits Window
-		Private salesPersonDataTable As New NwindDataSet.SalesPersonDataTable()
+
+'INSTANT VB NOTE: The variable salesPersonDataTable was renamed since it may cause conflicts with calls to static members of the user-defined type with this name:
+		Private salesPersonDataTable_Conflict As New NwindDataSet.SalesPersonDataTable()
 		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
 
 		Public Sub New()
 			InitializeComponent()
-			pivotGridControl1.DataSource = salesPersonDataTable
+			pivotGridControl1.DataSource = salesPersonDataTable_Conflict
 			AddHandler pivotGridControl1.GridMenu.Opened, AddressOf GridMenu_Opened
 		End Sub
 
@@ -29,7 +30,7 @@ Namespace HowToBindToMDB
 		End Sub
 
 		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			salesPersonDataAdapter.Fill(salesPersonDataTable)
+			salesPersonDataAdapter.Fill(salesPersonDataTable_Conflict)
 		End Sub
 	End Class
 End Namespace
