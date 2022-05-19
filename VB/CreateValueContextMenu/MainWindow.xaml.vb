68046873c51dfbab3412631311fbd6438c19697a
DevExpressExampleBot <fakeEmail>
2022-05-19 16:51:20 +0400

    Source auto update [skip ci]

--- VB/CreateValueContextMenu/MainWindow.xaml.vb
+++ VB/CreateValueContextMenu/MainWindow.xaml.vb
@@ -1,35 +1,33 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Data
-Imports System.Data.OleDb
 Imports System.Windows
 Imports DevExpress.Xpf.PivotGrid
 Imports HowToBindToMDB.NwindDataSetTableAdapters
 Imports DevExpress.Xpf.PivotGrid.Internal
 
 Namespace HowToBindToMDB
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Private salesPersonDataTable As New NwindDataSet.SalesPersonDataTable()
-		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
 
-		Public Sub New()
-			InitializeComponent()
-			pivotGridControl1.DataSource = salesPersonDataTable
-			AddHandler pivotGridControl1.GridMenu.Opened, AddressOf GridMenu_Opened
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub GridMenu_Opened(ByVal sender As Object, ByVal e As System.EventArgs)
-			If pivotGridControl1.GridMenu.MenuType <> PivotGridMenuType.HeadersArea Then
-				Return
-			End If
-			TestItem.IsVisible = CType(pivotGridControl1.GridMenu.PlacementTarget.GetValue(FieldHeaders.AreaProperty), FieldArea) = FieldArea.DataArea
-		End Sub
+        Private salesPersonDataTable As NwindDataSet.SalesPersonDataTable = New NwindDataSet.SalesPersonDataTable()
 
-		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			salesPersonDataAdapter.Fill(salesPersonDataTable)
-		End Sub
-	End Class
+        Private salesPersonDataAdapter As SalesPersonTableAdapter = New SalesPersonTableAdapter()
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.pivotGridControl1.DataSource = salesPersonDataTable
+            AddHandler Me.pivotGridControl1.GridMenu.Opened, New System.EventHandler(AddressOf GridMenu_Opened)
+        End Sub
+
+        Private Sub GridMenu_Opened(ByVal sender As Object, ByVal e As System.EventArgs)
+            If Me.pivotGridControl1.GridMenu.MenuType <> PivotGridMenuType.HeadersArea Then Return
+            Me.TestItem.IsVisible = CType(Me.pivotGridControl1.GridMenu.PlacementTarget.GetValue(FieldHeadersBase.AreaProperty), FieldArea) = FieldArea.DataArea
+        End Sub
+
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            salesPersonDataAdapter.Fill(salesPersonDataTable)
+        End Sub
+    End Class
 End Namespace
